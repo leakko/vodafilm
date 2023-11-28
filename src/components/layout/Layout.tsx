3cc75670f24c954c9ca59e3c228cfdeb6d32b0de
Marcos Rodriguez <marcos.rodriguez3@vodafone.com>
2023-11-28 15:46:19 +0100

    feat: :sparkles: center main section

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,17 +2,22 @@ import React, { Suspense } from 'react';
 import { Outlet } from 'react-router';
 import Footer from '../footer/Footer';
 import Header from '../header/Header';
-import Main from '../main/Main';
+import styled from 'styled-components';
+
+const StyledMain = styled.main`
+	max-width: 1000px;
+	margin: 25px auto;
+`;
 
 const Layout: React.FC = () => {
 	return (
 		<>
 			<Header />
-			<Main>
+			<StyledMain>
 				<Suspense fallback={<div>Loading...</div>}>
 					<Outlet />
 				</Suspense>
-			</Main>
+			</StyledMain>
 			<Footer />
 		</>
 	);
