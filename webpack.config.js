473239f5b3f3efeaaf945bfa4ec815d1cc923bf3
Marcos Rodriguez <marcos.rodriguez3@vodafone.com>
2023-11-30 14:47:34 +0100

    feat: :sparkles: add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,7 @@ module.exports = (env, arg) => {
 		plugins: [
 			new HtmlWebpackPlugin({
 				template: './public/index.html',
+				favicon: "./src/assets/img/favicon.ico"
 			}),
 			new Dotenv({
 				path: dotenvFilename,
